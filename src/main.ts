54d5871889c4896a55c7be988e5b41d12d093d27
Jacek Nowak <jaceknowak226@gmail.com>
2025-05-27 18:52:13 +0200

    ✨ feat: implement a basic EzFetch class with method chaining

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,46 @@
-export function add(a: number, b: number): number {
-  return a + b;
+type ObjectValues<T extends object> = T[keyof T];
+
+export const HttpMethod = {
+  GET: 'GET',
+  POST: 'POST',
+  PUT: 'PUT',
+  PATCH: 'PATCH',
+  DELETE: 'DELETE'
+} as const;
+
+export type HttpMethodValue = ObjectValues<typeof HttpMethod>;
+
+export type EzFetchResponse = {
+  ok: boolean;
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+type EzFetchInput = {
+  baseUrl: string;
+  method: HttpMethodValue;
 }
+
+class EzFetch<T extends Partial<EzFetchInput>> {
+  #actual: T;
+
+  private constructor(actual: T) {
+    this.#actual = actual;
+  }
+
+  static create(baseUrl: string) {
+    return new EzFetch({ baseUrl });
+  }
+
+  setMethod(method: HttpMethodValue) {
+    return new EzFetch({ ...this.#actual, method });
+  }
+
+  send(this: EzFetch<EzFetchInput>): EzFetchResponse {
+    return {
+      ok: true,
+    }
+  }
+}
+
+export const ezfetch = EzFetch;
+
+export default ezfetch;
