134e0a535f95199a381b72f4fda5b4180304149f
Jacek Nowak <jaceknowak226@gmail.com>
2025-05-27 19:49:10 +0200

    ✨ feat: add `json` method with basic functionality

--- src/main.ts
+++ src/main.ts
@@ -40,8 +40,20 @@ class EzFetch<T extends Partial<EzFetchInput>> {
     return new EzFetch({
       ...this.#actual,
       method: HttpMethod.GET,
-      url: this.#actual.url ? `${this.#actual.url}${url}` : url,
+      url: `${this.#actual.url ?? ""}${url}`,
     });
+  }
+
+  async json<V>(this: EzFetch<EzFetchInput>): Promise<V> {
+    const response = await fetch(this.#actual.url, {
+      method: this.#actual.method,
+    });
+
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    return await response.json() as V;
   }
 
   send(this: EzFetch<EzFetchInput>): EzFetchResponse {
